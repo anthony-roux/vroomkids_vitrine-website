d984013556a9f1664bbe40aa0e4f5b3588df71bf
Anthony Roux <anthony.roux@colorz.fr>
2022-05-06 17:15:24 +0200

    Fix bug opacity for firefox

--- components/Base/BigLetter.vue
+++ components/Base/BigLetter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center base-big-letter">
-    <span class="text-5xl font-black text-center text-transparent break-all base-big-letter__item md:text-8xl lg:text-9xl bg-clip-text bg-gradient-to-b from-primary-vert1 to-pink animate-text opacity-10">{{
+    <span class="text-5xl font-black text-center text-transparent break-all base-big-letter__item md:text-8xl lg:text-9xl bg-clip-text bg-gradient-to-b from-primary-vert1 to-pink animate-text opacity-30 ">{{
       text
     }}</span>
   </div>
@@ -20,4 +20,10 @@ export default {
 </script>
 
 <style lang="scss">
+.base-big-letter {
+  &__item {
+    opacity: 0.3 !important;
+    -moz-opacity:0.5 !important;
+  }
+}
 </style>
