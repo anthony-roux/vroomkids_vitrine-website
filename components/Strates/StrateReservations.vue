7e2aa37ed485a1dac52b4d80c061bc30b161f30a
Anthony Roux <anthony.roux@colorz.fr>
2022-05-11 22:59:02 +0200

    MAJ Strate reservation for best adaptability

--- components/Strates/StrateReservations.vue
+++ components/Strates/StrateReservations.vue
@@ -1,56 +1,117 @@
 <template>
   <section class="w-full my-32 md:my-60 strate-reservations">
     <div class="container mx-auto">
-      <p
+      <p v-if="title"
         class="mx-auto mb-16 text-3xl font-bold text-center lg:mb-32 strate-big-title__text md:text-5xl"
       >
         {{ title }}
       </p>
-      <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      <div
+        class="flex flex-col flex-wrap items-start justify-evenly lg:flex-row"
+      >
         <div
-          class="relative flex justify-center w-3/4 py-12 mx-auto mt-24 text-sm lg:mt-0"
+          v-if="text1"
+          class="
+            relative
+            flex
+            justify-center
+            py-12
+            mx-auto
+            mt-24
+            text-sm
+            4xl:mt-0
+            max-w-[28rem]
+          "
         >
           <div class="absolute top-[-6rem]">
             <slot name="text1"></slot>
           </div>
-          <p class="text-center">
-            Précisez simplement vos coordonnées personnelles ainsi que les
-            informations relatives à votre trajet dans le formulaire.
-          </p>
+          <div class="flex-col">
+            <p v-if="subtitle1" class="text-xl font-bold text-center">
+              {{ subtitle1 }}
+            </p>
+            <p class="text-center">
+              {{ text1 }}
+            </p>
+          </div>
         </div>
         <div
-          class="relative flex justify-center w-3/4 py-12 mx-auto mt-24 text-sm lg:mt-0"
+          v-if="text2"
+          class="
+            relative
+            flex
+            justify-center
+            py-12
+            mx-auto
+            mt-24
+            text-sm
+            4xl:mt-0
+            max-w-[28rem]
+          "
         >
           <div class="absolute top-[-6rem]">
             <slot name="text2"></slot>
           </div>
-
-          <p class="text-center">
-            Vous recevrez un email avec la proposition de prix ainsi qu’un
-            récapitulatif de votre demande. Vous acceptez la course.
-          </p>
+          <div class="flex-col">
+            <p v-if="subtitle2" class="text-xl font-bold text-center">
+              {{ subtitle2 }}
+            </p>
+            <p class="text-center">
+              {{ text2 }}
+            </p>
+          </div>
         </div>
         <div
-          class="relative flex justify-center w-3/4 py-12 mx-auto mt-24 text-sm lg:mt-0"
+          v-if="text3"
+          class="
+            relative
+            flex
+            justify-center
+            py-12
+            mx-auto
+            mt-24
+            text-sm
+            4xl:mt-0
+            max-w-[28rem]
+          "
         >
           <div class="absolute top-[-6rem]">
             <slot name="text3"></slot>
           </div>
-          <p class="text-center">
-            Nous sélectionnons le chauffeur et le véhicule le plus adapté à vos
-            besoins et à celui de l’enfant. Les courses peuvent commencer.
-          </p>
+          <div class="flex-col">
+            <p v-if="subtitle3" class="text-xl font-bold text-center">
+              {{ subtitle3 }}
+            </p>
+            <p class="text-center">
+              {{ text3 }}
+            </p>
+          </div>
         </div>
         <div
-          class="relative flex justify-center w-3/4 py-12 mx-auto mt-24 text-sm lg:mt-0"
+          v-if="text4"
+          class="
+            relative
+            flex
+            justify-center
+            py-12
+            mx-auto
+            mt-24
+            text-sm
+            4xl:mt-0
+            max-w-[28rem]
+          "
         >
           <div class="absolute top-[-6rem]">
             <slot name="text4"></slot>
           </div>
-          <p class="text-center">
-            Le chauffeur vient chercher le(s) enfant(s) et les accompagne
-            jusqu’à la prise en charge par un adulte.
-          </p>
+          <div class="flex-col">
+            <p v-if="subtitle4" class="text-xl font-bold text-center">
+              {{ subtitle4 }}
+            </p>
+            <p class="text-center">
+              {{ text4 }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -63,7 +124,46 @@ export default {
     title: {
       type: String,
       required: false,
-      default: "Duis esse est adipisicing elit veniam",
+    },
+    subtitle1: {
+      type: String,
+      required: false,
+    },
+    subtitle2: {
+      type: String,
+      required: false,
+    },
+    subtitle3: {
+      type: String,
+      required: false,
+    },
+    subtitle4: {
+      type: String,
+      required: false,
+    },
+    text1: {
+      type: String,
+      required: false,
+      default:
+        "Précisez simplement vos coordonnées personnelles ainsi que les informations relatives à votre trajet dans le formulaire.",
+    },
+    text2: {
+      type: String,
+      required: false,
+      default:
+        "Vous recevrez un email avec la proposition de prix ainsi qu’un récapitulatif de votre demande. Vous acceptez la course.",
+    },
+    text3: {
+      type: String,
+      required: false,
+      default:
+        "Nous sélectionnons le chauffeur et le véhicule le plus adapté à vos besoins et à celui de l’enfant. Les courses peuvent commencer.",
+    },
+    text4: {
+      type: String,
+      required: false,
+      default:
+        "Le chauffeur et l'accompagnateur viennent chercher le(s) enfant(s) et les accompagne jusqu’à la prise en charge par un adulte.",
     },
   },
   data: function () {
