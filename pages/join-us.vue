4ac5fa620f9e097ebeeff576bafea72b1066b176
dorian <dorian.vidal@hetic.net>
2022-06-27 14:40:55 +0200

    add page join us

---
+++ pages/join-us.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="page-driver" class="">
+    <strate-hero
+      title="Nous rejoindre"
+      ctaLabel
+      backgroundUrl="/img/photography/hero_others.png"
+    />
+    <strate-text
+      text="Vroom vous assure un service complet et entièrement sécurisé. Nous choisissons rigoureusement nos chauffeurs et nous assurons un accompagnement en toute confiance lors des trajets de vos enfants."
+      title="Pourquoi rejoindre l’expérience Vroom ?"
+    />
+    <strate-garantee />
+    <strate-manage-race />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "JoinUs",
+
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
