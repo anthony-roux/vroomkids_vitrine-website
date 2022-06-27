118a6aade918f9a3a47a033b9dda16ce8d411a9a
dorian <dorian.vidal@hetic.net>
2022-06-27 10:50:31 +0200

    fix bug

--- plugins/components.js
+++ plugins/components.js
@@ -76,7 +76,7 @@ Vue.component("strate-cities", StrateCities);
 Vue.component("strate-text", StrateText);
 Vue.component("strate-faq", StrateFAQ);
 Vue.component("strate-testimonials-pro", StrateTestimonialsPro);
-Vue.component("strate-member", Member);
+Vue.component("strate-member", StrateMember);
 
 // COMPONENTS
 
