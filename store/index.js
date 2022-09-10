8aa868e5258575783701c77c44c0dc673cdc1211
Anthony Roux <anthony.roux@colorz.fr>
2022-09-10 20:39:26 +0200

    set state empty

--- store/index.js
+++ store/index.js
@@ -4,10 +4,10 @@ import global from "./global";
 
 const storeApp = () => {
   return new Vuex.Store({
+    state: {},
     actions: {
       async nuxtServerInit({ dispatch, commit }, context) {
         await commit("global/setConfig"); // load config js
-
       },
     },
     namespaced: true,
